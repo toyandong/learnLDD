9cac9b7b60feb633aec895643d4e0fc698dd4e16
toyandong <toyandong@gmail.com>
2016-10-15 09:56:00 -0400

    use ioperm to test parport
    Signed-off-by: toyandong <toyandong@gmail.com>

--- parport/ioperm/ioperm.c
+++ parport/ioperm/ioperm.c
@@ -3,14 +3,18 @@
 #define BASEADDR 0x378 
 int main(int argc, char **argv) 
 { 
-	int i; 
+	char val;
 	if(ioperm(BASEADDR, 4, 1) < 0) {
 		fprintf(stderr, "could not get i/o permission. are you root ?\n"); 
 		return 5; 
 	} 
-	for(i=0; i < 256; i++) { 
-		outb(i, BASEADDR); 
-	} 
+	outb(0x1f, BASEADDR);
+	val = inb(BASEADDR);
+	printf("VAL is %x\n", val);
+	outb(0xff, BASEADDR);
+	val = inb(BASEADDR);
+	printf("VAL is %x\n", val);
+ 
 	ioperm(BASEADDR, 4, 0); 
 	return 0; 
 }
